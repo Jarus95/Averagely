acbc6b0f88166a38f874e64e4c57070b11be89f5
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-07-23 16:47:38 +0500

    Make variable lowercase

--- Program.cs
+++ Program.cs
@@ -5,24 +5,37 @@ namespace Averagely
     {
         static void Main()
         {
-           Console.WriteLine("The average Calculator");
-           Console.Write("Mathematics grade: ");
-           decimal Mathematics = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("The average Calculator");
+            Console.Write("Mathematics grade: ");
+            decimal mathematics = Convert.ToDecimal(Console.ReadLine());
 
-           Console.Write("Biology grade: ");
-           decimal Biology = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Biology grade: ");
+            decimal biology = Convert.ToDecimal(Console.ReadLine());
 
             Console.Write("Art grade: ");
-            decimal Art = Convert.ToDecimal(Console.ReadLine());
+            decimal art = Convert.ToDecimal(Console.ReadLine());
 
             Console.Write("Chesmistry grade: ");
-            decimal Chesmistry = Convert.ToDecimal(Console.ReadLine());
+            decimal chesmistry = Convert.ToDecimal(Console.ReadLine());
 
             Console.Write("Sciences grade: ");
-            decimal Sciences = Convert.ToDecimal(Console.ReadLine());
+            decimal sciences = Convert.ToDecimal(Console.ReadLine());
 
-            decimal avg = (Mathematics + Biology + Art + Chesmistry + Sciences)/5;
-            Console.WriteLine("Average Grade: " + avg);
+            decimal average = (mathematics + biology + art + chesmistry + sciences)/5;
+            Console.WriteLine("Average Grade: " + average);
+            Console.WriteLine(new string('-', 40));
+            
+            if(average > 4 && average <=5 )
+                Console.WriteLine("Yoe are an Exellent student");
+                
+            else if(average <= 4 && average > 3)
+                Console.WriteLine("Yoe are a good student");
+
+            else if(average > 0 && average <=3)
+               Console.WriteLine("Yoe are a bad student");
+
+            else
+               Console.WriteLine("Invalid Input");
         }
     }
 }
